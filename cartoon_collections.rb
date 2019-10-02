302ef6cfa641f333aa1e31004997e274b85c3df9
Kuran V Kumar <kurankumar@gmail.com>
2019-10-02 23:15:59 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  i = 0 
+  
+  while i < dwarf_names.length
+  puts dwarf_names[i]
+  i += 1 
+  
 end
 
 def summon_captain_planet# code an argument here
